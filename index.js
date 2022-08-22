62e6bcd054b4869af4a5faea4beb7db55b693f8a
Curtrick Walton <curtrickw@yahoo.com>
2022-08-22 14:11:51 -0500

    test

--- index.js
+++ index.js
@@ -29,14 +29,21 @@ app.get('/starwars',(req,res) => {
 
 
 app.get('/starwars/:id',(req,res) => {
-    res.send(
-        req.params.id
-    )
+    if(req.params.id !== leia) {
+        res.send(400)
+    }else{
+        res.send({
+       characterName: 'Leia',
+        characterAge: 95.5,
+        light_saber: 'blue',
+        dad: ' The god Guy',
+        })
+    }
+    // res.send(
+    //     req.params.id
+    // )
 })
-// characterName: 'Leia',
-//         characterAge: 95.5,
-//         light_saber: 'blue',
-//         dad: ' The god Guy',
+
 
 
 
