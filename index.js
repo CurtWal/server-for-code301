c2e7a3fa1c2981d9195948ddeca3dc54241e01ff
Curtrick Walton <curtrickw@yahoo.com>
2022-08-22 13:47:15 -0500

    test

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ app.get('/',(req,res) => {
     res.send('Hello World');
 })
 
-
+// send the use a StarWars Character
+app.get('/startwars',(req,res) => {
+    res.send({
+        characterName: 'Luke Skywalker',
+        characterAge: 95,
+        light_saber: 'green',
+        dad: 'The bad guy',
+        quip: 'May the force be with you....'
+    })
+})
 
 
 
